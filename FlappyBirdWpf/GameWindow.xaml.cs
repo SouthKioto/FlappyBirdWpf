09362a6527a5b9c6af8d4c5039942f43768e2733
ImColdBlooded <bolisegabartek6@gmail.com>
2024-01-20 13:54:46 +0100

    wprowadzenie sprawdzania kliknięcia spacji aby rozpocząć grę

--- FlappyBirdWpf/GameWindow.xaml.cs
+++ FlappyBirdWpf/GameWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace FlappyBirdWpf
 
         private double birdPositionY = 0;
 
+        private bool isSpacePressed = false;
+
         private DispatcherTimer timer;
         private TimeSpan elapsedTime;
 
@@ -38,42 +40,48 @@ namespace FlappyBirdWpf
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            elapsedTime = elapsedTime.Add(TimeSpan.FromMilliseconds(60));
-
-            birdPositionY += gravity;
-            Canvas.SetTop(birdImage, birdPositionY);
-
-            Rect birdRect = new Rect(Canvas.GetLeft(birdImage), birdPositionY, birdImage.ActualWidth, birdImage.ActualHeight);
-            Rect groundRect = new Rect(Canvas.GetLeft(ground), Canvas.GetTop(ground), ground.ActualWidth, ground.ActualHeight);
-            Rect pipeTopRect = new Rect(Canvas.GetLeft(pipeTop), Canvas.GetTop(pipeTop), pipeTop.ActualWidth, pipeTop.ActualHeight);
-            Rect pipeDownRect = new Rect(Canvas.GetLeft(pipeDown), Canvas.GetTop(pipeDown), pipeDown.ActualWidth, pipeDown.ActualHeight);
-
-            if (birdRect.IntersectsWith(groundRect) || birdRect.IntersectsWith(pipeTopRect) || birdRect.IntersectsWith(pipeDownRect))
+            if (isSpacePressed)
             {
-                GameOver();
+                elapsedTime = elapsedTime.Add(TimeSpan.FromMilliseconds(60));
+
+                birdPositionY += gravity;
+                Canvas.SetTop(birdImage, birdPositionY);
+
+                Rect birdRect = new Rect(Canvas.GetLeft(birdImage), birdPositionY, birdImage.ActualWidth, birdImage.ActualHeight);
+                Rect groundRect = new Rect(Canvas.GetLeft(ground), Canvas.GetTop(ground), ground.ActualWidth, ground.ActualHeight);
+                Rect pipeTopRect = new Rect(Canvas.GetLeft(pipeTop), Canvas.GetTop(pipeTop), pipeTop.ActualWidth, pipeTop.ActualHeight);
+                Rect pipeDownRect = new Rect(Canvas.GetLeft(pipeDown), Canvas.GetTop(pipeDown), pipeDown.ActualWidth, pipeDown.ActualHeight);
+
+                if (birdRect.IntersectsWith(groundRect) || birdRect.IntersectsWith(pipeTopRect) || birdRect.IntersectsWith(pipeDownRect))
+                {
+                    GameOver();
+                }
+
+
+                if (Canvas.GetLeft(pipeTop) + pipeTop.ActualWidth < 0)
+                {
+                    Canvas.SetLeft(pipeTop, this.Width);
+                    Canvas.SetLeft(pipeDown, this.Width);
+
+                    Random random = new Random();
+                    double randomHeight = random.Next(-50, (int)this.Height - pipeGap - 50);
+                    Canvas.SetTop(pipeTop, randomHeight - pipeTop.ActualHeight);
+                    Canvas.SetTop(pipeDown, randomHeight + pipeGap);
+                }
+
+                Canvas.SetLeft(pipeTop, Canvas.GetLeft(pipeTop) - pipeSpeed);
+                Canvas.SetLeft(pipeDown, Canvas.GetLeft(pipeDown) - pipeSpeed);
+
+
+                timerLabel.Content = elapsedTime.ToString("mm':'ss");
             }
-
-            if (Canvas.GetLeft(pipeTop) + pipeTop.ActualWidth < 0)
-            {
-                Canvas.SetLeft(pipeTop, this.Width);
-                Canvas.SetLeft(pipeDown, this.Width);
-
-                Random random = new Random();
-                double randomHeight = random.Next(-50, (int)this.Height - pipeGap - 50);
-                Canvas.SetTop(pipeTop, randomHeight - pipeTop.ActualHeight);
-                Canvas.SetTop(pipeDown, randomHeight + pipeGap);
-            }
-
-            Canvas.SetLeft(pipeTop, Canvas.GetLeft(pipeTop) - pipeSpeed);
-            Canvas.SetLeft(pipeDown, Canvas.GetLeft(pipeDown) - pipeSpeed);
-
-            timerLabel.Content = elapsedTime.ToString("mm':'ss");
         }
 
         private void BirdUp_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space)
             {
+                isSpacePressed = true;
                 gravity = -15;
             }
         }
@@ -102,6 +110,7 @@ namespace FlappyBirdWpf
                 gravity = 5;
                 elapsedTime = TimeSpan.Zero;
                 timerLabel.Content = "00:00";
+                isSpacePressed = false;
                 timer.Start();
             }
             if (answer == MessageBoxResult.No)
